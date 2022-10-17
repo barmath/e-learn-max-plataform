bc08da43b1a036347afdc917e02df63789aed186
mateusmss <mateusaldanha@usp.br>
2022-10-16 21:30:23 -0300

    Fix de problem with webconsole e testes do cucumber funcionando

---
+++ features/step_definitions/perguntas_steps.rb
@@ -0,0 +1,45 @@
+Dado('que estou na página de nova pergunta') do
+  visit '/questaos/new'
+  click_on: 
+end
+
+Quando('preencho o campo de pergunta com {string}') do |string|
+  
+end
+
+Quando('aperto o botão Salvar Pergunta') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('deve pedir confirmação para salvar a pergunta') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('ele deve salvar a questão no banco de dados') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('que estou na tela de Nova Pergunta') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('aperto o botão Cancelar') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('deverei ver de confirmação {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('deixo o campo “Questão {int}” vazio') do |int|
+  # Quando('deixo o campo “Questão {float}” vazio') do |float|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('quando aperto o botão Salvar Pergunta') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('deverei ver a mensagem de erro {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
