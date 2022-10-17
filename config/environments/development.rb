3eaf67eae9cedb8137eaf8a0a0f3bb6bfe59ca4a
mateusmss <mateusaldanha@usp.br>
2022-10-16 21:31:10 -0300

    fix de webconsole e desenvolvendo cenários do cucumber

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-  config.web_console.whiny_requests = false
+  
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
