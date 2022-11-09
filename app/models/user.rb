23875fe2f8566fcf141b14ef2ad521a18e081eb8
matheuscgh <matheus_barbosa@usp.br>
2022-11-09 12:27:08 -0300

    adding validadation so user dont stay without a role

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,19 @@ class User < ApplicationRecord
 
   after_create :assign_default_role
 
+  validate :must_have_a_role, on: :update 
+
+ 
+  private
+
   def assign_default_role
     self.add_role(:newuser) if self.roles.blank?
   end
+
+  def must_have_a_role
+    unless roles.any?
+      errors.add(:roles, 'must to have at least 1 role')
+    end
+  end 
+
 end
