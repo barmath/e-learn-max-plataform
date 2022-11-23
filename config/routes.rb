6154028f1ac68e5fcee6bcb81ec8fc2a763fe9a6
matheuscgh <matheus_barbosa@usp.br>
2022-11-23 16:17:06 -0300

    roles part

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :index 
   resources :users 
+  resources :professors 
+  resources :alunos 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
