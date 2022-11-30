5a475c5d39eeef26acd8297c9ebb582c816f23f7
matheuscgh <matheus_barbosa@usp.br>
2022-11-30 15:07:32 -0300

    shift from sql lite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_11_30_003907) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "provas", force: :cascade do |t|
     t.string "titulo"
     t.string "instrucoes"
@@ -29,7 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_003907) do
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "resource_type"
-    t.integer "resource_id"
+    t.bigint "resource_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
@@ -50,8 +53,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_003907) do
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "role_id"
+    t.bigint "user_id"
+    t.bigint "role_id"
     t.index ["role_id"], name: "index_users_roles_on_role_id"
     t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
     t.index ["user_id"], name: "index_users_roles_on_user_id"
