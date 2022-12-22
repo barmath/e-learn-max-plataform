f6d761fc5682b038140a248436c0e5a6c6b777dd
mateusmss <mateusaldanha@usp.br>
2022-12-21 22:11:00 -0300

    fixing provas tests

--- spec/views/provas/index.html.erb_spec.rb
+++ spec/views/provas/index.html.erb_spec.rb
@@ -15,9 +15,9 @@ RSpec.describe "provas/index", type: :view do
   end
 
   it "renders a list of provas" do
-  render
+    render
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
-    assert_select cell_selector, text: Regexp.new("Bem vindo ao portal do Admin".to_s), count: 1
-
+    assert_select cell_selector, text: Regexp.new("Titulo".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("Instrucoes".to_s), count: 2
   end
 end
